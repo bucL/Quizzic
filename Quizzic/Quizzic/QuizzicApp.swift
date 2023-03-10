7637fd3e7de57b58137fb01c8c981e1d173a3ce7
Aaditya Shankar <aadityashankar06@gmail.com>
2023-03-11 08:52:50 +1100

    Initialised Xcode

---
+++ Quizzic/Quizzic/QuizzicApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzicApp.swift
+//  Quizzic
+//
+//  Created by Aaditya Shankar on 11/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizzicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
